94ea1294840f3e855bae3b85d9ec6d9e324cc882
SustyDigiMarketers <sustydigimarketers@gmail.com>
2025-10-22 16:24:51 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: For deploying to a GitHub repository page, the base path
-  // must be the name of your repository.
-  base: '/Faustina-Easy-Education/',
+  // For deploying to a custom root domain, the base path must be '/'.
+  // For deploying to a GitHub repository subdirectory, it should be '/<repository-name>/'.
+  base: '/',
 })
